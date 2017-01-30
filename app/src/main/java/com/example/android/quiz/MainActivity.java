8f7866e36852f4b6fa15d545bafa7417bc489f50
Krzysztof Nawrocki <krzysztof.nawrocki79@gmail.com>
2017-01-30 21:12:59 +0100

    textedit and radio buttons

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -3,18 +3,29 @@ package com.example.android.quiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import static com.example.android.quiz.R.id.answer;
+import static com.example.android.quiz.R.id.radio_buttons;
 
 public class MainActivity extends AppCompatActivity {
 
     Question[] questions = {
-            new Question("Jak ma na imię nasza córka ? ",QuestionType.TEXT_EDIT,"Gabrysia"),
-            new Question("Ile zębów ma nasza córka ?",QuestionType.RADIO_BUTTON,"2"),
+            new Question("Jak ma na imię nasza córka ? ",QuestionType.TEXT_EDIT,"Gabi"),
+            new Question("Ile zębów ma nasza córka ?",QuestionType.RADIO_BUTTON,"2 zęby"),
             new Question("W co ubieramy naszą córkę ?",QuestionType.CHECK_BOX,"Rajstopy,Body")
     };
 
+    boolean[] answers = new boolean[3];
+
     View firstView, secondView, thirdView;
+    LinearLayout textEdit, radioButtons, checkBoxes;
     TextView questionField;
     int questionNumber = 0;
 
@@ -27,9 +38,17 @@ public class MainActivity extends AppCompatActivity {
         secondView = (View) findViewById(R.id.second_view);
         thirdView = (View) findViewById(R.id.third_view);
 
+        textEdit = (LinearLayout) findViewById(R.id.text_edit);
+        radioButtons = (LinearLayout) findViewById(R.id.radio_buttons);
+        checkBoxes = (LinearLayout) findViewById(R.id.check_boxes);
+
         firstView.setVisibility(View.VISIBLE);
         secondView.setVisibility(View.GONE);
         thirdView.setVisibility(View.GONE);
+
+        textEdit.setVisibility(View.VISIBLE);
+        radioButtons.setVisibility(View.GONE);
+        checkBoxes.setVisibility(View.GONE);
 
         questionField = (TextView) findViewById(R.id.question_field);
     }
@@ -45,12 +64,27 @@ public class MainActivity extends AppCompatActivity {
         questionField.setText(questions[questionNumber].getQuestion());
     }
 
-    private void nextQuestion() {
+    public void nextQuestion(View view) {
         if(questionNumber==0){
+            EditText answer = (EditText) findViewById(R.id.answer);
 
+            answers[0] = answer.getText().toString().equals(questions[questionNumber].getAnswer());
+
+            textEdit.setVisibility(View.GONE);
+            radioButtons.setVisibility(View.VISIBLE);
+            questionNumber+=1;
         }
         else if(questionNumber==1) {
+            RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group);
 
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+            RadioButton selectedRadioButton = (RadioButton) findViewById(selectedId);
+            String selectedValue = selectedRadioButton.getText().toString();
+            answers[1] = selectedValue.equals(questions[questionNumber].getAnswer());
+
+            radioButtons.setVisibility(View.GONE);
+            checkBoxes.setVisibility(View.VISIBLE);
+            questionNumber+=1;
         }
         else if(questionNumber==2) {
 
